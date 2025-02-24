a93a3c1f3345671f1be0e9b4a008fd2698930327
tv <tv104@users.noreply.github.com>
2025-02-24 20:29:55 +0100

    fix sticky renders suppressing onReady

--- src/components/panorama-viewer.tsx
+++ src/components/panorama-viewer.tsx
@@ -1,16 +1,16 @@
 import { ComponentProps } from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
-// import { EquirectangularAdapter } from "@photo-sphere-viewer/core";
 import { Box, ThemeUIStyleObject } from "theme-ui";
 
 type Props = {
   src: ComponentProps<typeof ReactPhotoSphereViewer>["src"];
-  preloadSrc: ComponentProps<typeof ReactPhotoSphereViewer>["src"];
+  preloadSrc?: ComponentProps<typeof ReactPhotoSphereViewer>["src"];
   onCurrentReady: () => void;
   onNextReady: () => void;
   roundActive: boolean;
   isTransitioningRound: boolean;
   onTransitionEnd: () => void;
+  gameCount: number;
 };
 
 export const PanoramaViewer: React.FC<Props> = ({
@@ -21,6 +21,7 @@ export const PanoramaViewer: React.FC<Props> = ({
   roundActive,
   isTransitioningRound,
   onTransitionEnd,
+  gameCount,
 }) => {
   const commonProps = {
     height: "100vh",
@@ -47,7 +48,7 @@ export const PanoramaViewer: React.FC<Props> = ({
       left: 0,
       right: 0,
       bottom: 0,
-      transition: "filter 0.5s ease-in",
+      transition: "filter 1.5s ease-in",
     },
     currentImage: {
       zIndex: 1,
@@ -63,29 +64,29 @@ export const PanoramaViewer: React.FC<Props> = ({
     <Box sx={styles.container}>
       <Box
         sx={{ ...styles.panoramaImage, ...styles.currentImage }}
-        key={src.toString()}
+        key={`${src}-${gameCount}`}
         onTransitionEnd={onTransitionEnd}
       >
         <ReactPhotoSphereViewer
           {...commonProps}
-          key={src.toString()}
           src={src}
           containerClass="current-pano-img"
           onReady={onCurrentReady}
         />
       </Box>
-      <Box
-        sx={{ ...styles.panoramaImage, ...styles.nextImage }}
-        key={preloadSrc.toString()}
-      >
-        <ReactPhotoSphereViewer
-          {...commonProps}
-          key={preloadSrc.toString()}
-          src={preloadSrc}
-          containerClass="next-pano-img"
-          onReady={onNextReady}
-        />
-      </Box>
+      {preloadSrc && (
+        <Box
+          sx={{ ...styles.panoramaImage, ...styles.nextImage }}
+          key={`${preloadSrc}-${gameCount}`}
+        >
+          <ReactPhotoSphereViewer
+            {...commonProps}
+            src={preloadSrc}
+            containerClass="next-pano-img"
+            onReady={onNextReady}
+          />
+        </Box>
+      )}
     </Box>
   );
 };
