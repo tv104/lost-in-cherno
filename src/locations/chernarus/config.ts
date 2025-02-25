62b704ddf1389a71fa6f49a68ac0070a80029910
tv <tv104@users.noreply.github.com>
2025-02-25 20:25:41 +0100

    add new panoramas

--- src/locations/chernarus/config.ts
+++ src/locations/chernarus/config.ts
@@ -1,53 +1,73 @@
-// aesthetic locations (ideally they are all aesthetic)
-import image6 from './panoramas/6.webp'
-import image7 from './panoramas/7.webp'
-import image12 from './panoramas/12.webp'
-import image16 from './panoramas/16.webp'
-import image18 from './panoramas/18.webp'
-import image20 from './panoramas/20.webp'
-import image21 from './panoramas/21.webp'
-import image27 from './panoramas/27.webp'
-import image32 from './panoramas/32.webp'
-// regular locations
 import image1 from './panoramas/1.webp'
-import image8 from './panoramas/8.webp'
-import image9 from './panoramas/9.webp'
 import image11 from './panoramas/11.webp'
+import image12 from './panoramas/12.webp'
 import image14 from './panoramas/14.webp'
 import image15 from './panoramas/15.webp'
+import image16 from './panoramas/16.webp'
 import image17 from './panoramas/17.webp'
+import image18 from './panoramas/18.webp'
 import image19 from './panoramas/19.webp'
+import image20 from './panoramas/20.webp'
+import image21 from './panoramas/21.webp'
 import image22 from './panoramas/22.webp'
 import image23 from './panoramas/23.webp'
 import image25 from './panoramas/25.webp' 
 import image26 from './panoramas/26.webp'
+import image27 from './panoramas/27.webp'
 import image28 from './panoramas/28.webp'
+import image32 from './panoramas/32.webp'
 import image33 from './panoramas/33.webp'
+import image34 from './panoramas/34.webp'
+import image35 from './panoramas/35.webp'
+import image36 from './panoramas/36.webp'
+import image37 from './panoramas/37.webp'
+import image38 from './panoramas/38.webp'
+import image39 from './panoramas/39.webp'
+import image40 from './panoramas/40.webp'
+import image41 from './panoramas/41.webp'
+import image42 from './panoramas/42.webp'
+import image44 from './panoramas/44.webp'
+import image46 from './panoramas/46.webp'
+import image6 from './panoramas/6.webp'
+import image7 from './panoramas/7.webp'
+import image8 from './panoramas/8.webp'
+import image9 from './panoramas/9.webp'
 
 import { PanoramaConfig } from '../../utils/panoramas'
 
 export const panoramas: PanoramaConfig[] = [
     { id: "1", image: image1, location: [0, 0] },
-    { id: "6", image: image6, location: [0, 0] },
-    { id: "7", image: image7, location: [0, 0] },
+    { id: "6", image: image6, location: [0, 0] }, 
+    { id: "7", image: image7, location: [0, 0] }, 
     { id: "8", image: image8, location: [0, 0] },
     { id: "9", image: image9, location: [0, 0] },
     { id: "11", image: image11, location: [0, 0] },
-    { id: "12", image: image12, location: [0, 0] },
+    { id: "12", image: image12, location: [0, 0] }, 
     { id: "14", image: image14, location: [0, 0] },
     { id: "15", image: image15, location: [0, 0] },
-    { id: "16", image: image16, location: [0, 0] },
+    { id: "16", image: image16, location: [0, 0] }, 
     { id: "17", image: image17, location: [0, 0] },
-    { id: "18", image: image18, location: [0, 0] },
+    { id: "18", image: image18, location: [0, 0] }, 
     { id: "19", image: image19, location: [0, 0] }, 
-    { id: "20", image: image20, location: [0, 0] },
+    { id: "20", image: image20, location: [0, 0] }, 
     { id: "21", image: image21, location: [0, 0] }, 
     { id: "22", image: image22, location: [0, 0] },
     { id: "23", image: image23, location: [0, 0] },
     { id: "25", image: image25, location: [0, 0] },
     { id: "26", image: image26, location: [0, 0] },
-    { id: "27", image: image27, location: [0, 0] },
+    { id: "27", image: image27, location: [0, 0] }, 
     { id: "28", image: image28, location: [0, 0] },
-    { id: "32", image: image32, location: [0, 0] },
-    { id: "33", image: image33, location: [0, 0] }
+    { id: "32", image: image32, location: [0, 0] }, 
+    { id: "33", image: image33, location: [0, 0] },
+    { id: "34", image: image34, location: [0, 0] }, 
+    { id: "35", image: image35, location: [0, 0] },
+    { id: "36", image: image36, location: [0, 0] },
+    { id: "37", image: image37, location: [0, 0] },
+    { id: "38", image: image38, location: [0, 0] },
+    { id: "39", image: image39, location: [0, 0] },
+    { id: "40", image: image40, location: [0, 0] }, 
+    { id: "41", image: image41, location: [0, 0] },
+    { id: "42", image: image42, location: [0, 0] },
+    { id: "44", image: image44, location: [0, 0] },
+    { id: "46", image: image46, location: [0, 0] },
 ]
