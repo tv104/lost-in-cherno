211cf5f78e01db3f63dd50226ba3a0a240271d2d
tv <tv104@users.noreply.github.com>
2025-04-16 21:50:42 +0200

    migrate remaining guessmap

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-12 rounded-md px-6 has-[>svg]:px-4 font-semibold",
+        lg: "h-[var(--button-height)] rounded-md px-6 has-[>svg]:px-4 font-semibold",
         icon: "size-9",
       },
     },
